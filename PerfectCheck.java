78b15f9f067bb5adda30fdd2074409dcc26ee100
SteveAnttoD <steveantto@karunya.edu.in>
2025-09-08 01:12:52 +0530

    Add files via upload

---
+++ PerfectCheck.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PerfectCount{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: ");
+        String input = sc.nextLine();
+        String lowerInput = input.toLowerCase();
+
+        if (hasUniqueCharacters(lowerInput)) {
+            System.out.println("Output: PERFECT");
+        } else {
+            System.out.println("Output: NOT PERFECT");
+            char repeatedChar = findFirstRepeatedChar(lowerInput);
+            System.out.println("[Reason: \"" + input + "\" has '" + repeatedChar + "' as repeating character]");
+        }
+        sc.close();
+    }
+
+    public static boolean hasUniqueCharacters(String str) {
+        boolean[] charSet = new boolean[256];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') continue;
+            if (charSet[ch]) return false;
+            charSet[ch] = true;
+        }
+        return true;
+    }
+
+    public static char findFirstRepeatedChar(String str) {
+        boolean[] charSet = new boolean[256];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') continue;
+            if (charSet[ch]) return ch;
+            charSet[ch] = true;
+        }
+        return ' ';
+    }
+}
